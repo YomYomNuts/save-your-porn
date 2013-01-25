f3bd3ba9ac184512f4b61ec13c878ddf0a81780c
marechalbanane <marechalbanane@gmail.com>
2013-01-25 22:57:39 +0000

    add dispersion, to measure if the player has a very wide input

--- Assets/Code/StaticMassageObjective.cs
+++ Assets/Code/StaticMassageObjective.cs
@@ -4,15 +4,16 @@ using System.Collections;
 /* objectif de massage statique : on doit appuyer plein de fois sur une zone donnée jusqu'à remplir une jauge */
 public class StaticMassageObjective : MonoBehaviour
 {
+	/* user settable */
 	public Vector2 m_PositionToReach;
-	
-	/* counting achievement percentage */
 	public float 	m_ValueToReach;
-	private	float	m_CurrentValue;		
+	public float	m_Frequency;
 	
+	private	float	m_CurrentValue;		
 	private bool 	m_recording;
 	private Vector2 m_CurrentPos;
 	private int		m_CurrentNbKeys;
+	private float	m_CurrentDispersion;
 
 	// Use this for initialization
 	void Start ()
@@ -33,12 +34,13 @@ public class StaticMassageObjective : MonoBehaviour
 		InputAnalyzer inputAn = InputAnalyzer.GetInstance();
 		int nbKeys = inputAn.GetNbKeysPressed();
 		Vector2 pos = inputAn.GetInputPos();
+		float dispersion = inputAn.GetDispersion();
 		if (nbKeys > 0)
 		{
 			if (!m_recording)
-				StartRecord(pos, nbKeys);
+				StartRecord(pos, nbKeys, dispersion);
 			else
-				UpdateRecord(pos, nbKeys);
+				UpdateRecord(pos, nbKeys, dispersion);
 		}
 		else if (m_recording)
 		{
@@ -46,19 +48,21 @@ public class StaticMassageObjective : MonoBehaviour
 		}
 	}
 	
-	private void StartRecord(Vector2 position, int nbKeys)
+	private void StartRecord(Vector2 position, int nbKeys, float dispersion)
 	{
 		m_recording = true;
 		m_CurrentNbKeys = nbKeys;
 		m_CurrentPos = position;
+		m_CurrentDispersion = dispersion;
 	}
 	
-	private void UpdateRecord(Vector2 position, int nbKeys)
+	private void UpdateRecord(Vector2 position, int nbKeys, float dispersion)
 	{
 		if (nbKeys >= m_CurrentNbKeys)
 		{
 			m_CurrentNbKeys = nbKeys;
 			m_CurrentPos = position;
+			m_CurrentDispersion = dispersion;
 		}
 	}
 	
@@ -70,8 +74,10 @@ public class StaticMassageObjective : MonoBehaviour
 	private void EndRecord()
 	{
 		m_recording = false;
-		m_CurrentValue += m_CurrentNbKeys * m_CurrentNbKeys / (1 + (m_PositionToReach - m_CurrentPos).sqrMagnitude);
 		
+		/* compute score for current massage and add to score */
+		float scoreToAdd = m_CurrentNbKeys * m_CurrentNbKeys * m_CurrentDispersion / (1 + (m_PositionToReach - m_CurrentPos).sqrMagnitude);
+		m_CurrentValue += scoreToAdd;
 		
 		if (m_CurrentValue >= m_ValueToReach)
 		{
@@ -84,6 +90,8 @@ public class StaticMassageObjective : MonoBehaviour
 			float newScale = (m_ValueToReach - m_CurrentValue) / m_ValueToReach;
 			transform.localScale = new Vector3(newScale, newScale, 1);
 			Debug.Log("Current value : " + m_CurrentValue);
+			Debug.Log("Added : " + scoreToAdd);
+			Debug.Log("Dispersion : " + m_CurrentDispersion);
 		}
 	}
 }
