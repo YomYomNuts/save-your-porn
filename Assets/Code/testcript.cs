6420c7b8b8aed77cf9ff4b3aac6a711eeeea9942
marechalbanane <marechalbanane@gmail.com>
2013-01-25 19:53:32 +0000

    ADD CODE FOLDER

---
+++ Assets/Code/testcript.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+
+public class testcripts : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
