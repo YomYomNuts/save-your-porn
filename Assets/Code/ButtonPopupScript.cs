26dc06b9452f496f6f1962eb28de8198722cab58
noisette.guillaume <noisette.guillaume@b7f410d1-8794-cbab-fdf4-a3e517f50e5e>
2013-01-26 21:37:53 +0000

    Limitation sur la hauteur de l'écran + tri en Z

--- Assets/Code/ButtonPopupScript.cs
+++ Assets/Code/ButtonPopupScript.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class ButtonPopupScript : MonoBehaviour {
 	//Attributs
+	public Transform transformDesktop;
 	public bool launchActionOnMouseEnter = false;
 	public Const.ACTION_TYPE actionButton = Const.ACTION_TYPE.CLOSE;
 	public Vector3 maxShift = new Vector3(15, 15, 15);
@@ -26,7 +27,7 @@ public class ButtonPopupScript : MonoBehaviour {
 	// Quand la souris passe sur le bouton, on exécute les actions adéquates
 	void OnMouseEnter()
 	{
-		if (this.launchActionOnMouseEnter)
+		if (this.launchActionOnMouseEnter && isClosest())
 			StartCoroutine("doActionButton");
 	}
 	
@@ -35,73 +36,86 @@ public class ButtonPopupScript : MonoBehaviour {
 		//Ajout du listener
 		this.GetComponent<ClickListenerScript>().OnClicked += new ClickListenerScript.ActionEventClick(
 		() => {
-			if (!this.launchActionOnMouseEnter)
+			if (!this.launchActionOnMouseEnter && isClosest())
 				StartCoroutine("doActionButton");
 		});
+	}
+	
+	// Savoir si c'est le plus proche de la caméra
+	public bool isClosest()
+	{
+		foreach(GameObject g in FindObjectsOfType(typeof(GameObject)))
+		{
+			if (g.layer == Const.LAYER_DESKTOP && g.renderer != null && g.transform.position.z > this.transform.position.z)
+				return false;
+		}
+		return true;
 	}
 	
 	// Action of the button
 	public IEnumerator doActionButton()
 	{
-		PopupScript thisPopup = this.transform.parent.gameObject.GetComponent<PopupScript>();
-		if (thisPopup.counterChildPopup == 0)
+		Const.ACTION_TYPE currentAction = this.actionButton;
+		if (this.counterTransition == this.numberOfClickLaunchSecondAction)
+			currentAction = this.secondActionButton;
+		
+		switch(currentAction)
 		{
-			Const.ACTION_TYPE currentAction = this.actionButton;
-			if (this.counterTransition == this.numberOfClickLaunchSecondAction)
-				currentAction = this.secondActionButton;
-			
-			switch(currentAction)
+			case Const.ACTION_TYPE.CLOSE:
 			{
-				case Const.ACTION_TYPE.CLOSE:
-				{
-					if (thisPopup.parentPopup != null)
-						thisPopup.parentPopup.counterChildPopup--;
-        			Destroy(this.transform.parent.gameObject);
-					break;
-				}
-				case Const.ACTION_TYPE.POP_SAME:
-				{
-					string namePrefab = this.transform.parent.gameObject.name.Replace("(Clone)", "");
-					GameObject _popup = Resources.Load("Prefabs/Popups/" + namePrefab) as GameObject;
-					popPopup(thisPopup, _popup);
-					break;
-				}
-				case Const.ACTION_TYPE.POP_RANDOM:
-				{
-					Object[] _popups = Resources.LoadAll("Prefabs/Popups");
-					
-					for (int i = 0; i < numberOfPopupLaunch; i++)
-					{
-						popPopup(thisPopup, _popups[Random.Range(0, _popups.Length)]);
-						yield return new WaitForSeconds(speedAppararitionPopup);
-					}
-					break;
-				}
-				case Const.ACTION_TYPE.BLACK_SCREEN:
-				{
-					foreach(GameObject g in FindObjectsOfType(typeof(GameObject)))
-					{
-						if (g.layer == Const.LAYER_DESKTOP && g.renderer != null)
-							g.renderer.enabled = false;
-					}
-					break;
-				}
+    			Destroy(this.transform.parent.gameObject);
+				break;
 			}
-			this.counterTransition++;
+			case Const.ACTION_TYPE.POP_SAME:
+			{
+				string namePrefab = this.transform.parent.gameObject.name.Replace("(Clone)", "");
+				GameObject _popup = Resources.Load("Prefabs/Popups/" + namePrefab) as GameObject;
+				popPopup(_popup);
+				break;
+			}
+			case Const.ACTION_TYPE.POP_RANDOM:
+			{
+				Object[] _popups = Resources.LoadAll("Prefabs/Popups");
+				
+				for (int i = 0; i < numberOfPopupLaunch; i++)
+				{
+					popPopup((_popups[Random.Range(0, _popups.Length - 1)] as GameObject));
+					yield return new WaitForSeconds(speedAppararitionPopup);
+				}
+				break;
+			}
+			case Const.ACTION_TYPE.BLACK_SCREEN:
+			{
+				foreach(GameObject g in FindObjectsOfType(typeof(GameObject)))
+				{
+					if (g.layer == Const.LAYER_DESKTOP && g.renderer != null)
+						g.renderer.enabled = false;
+				}
+				break;
+			}
 		}
+		this.counterTransition++;
 	}
 	
 	// Pop a popup
-	public void popPopup(PopupScript thisPopup, Object objectToPop)
+	public void popPopup(GameObject objectToPop)
 	{
-		Vector3 newPosition = new Vector3(
-				this.transform.parent.position.x + Random.Range(-this.maxShift.x, this.maxShift.x),
-				this.transform.parent.position.y + Random.Range(-this.maxShift.y, this.maxShift.y),
-				this.transform.parent.position.z + Random.Range(0, this.maxShift.z));
-	
+		float posX = this.transform.parent.position.x + Random.Range(-this.maxShift.x, this.maxShift.x);
+		float posY = this.transform.parent.position.y + Random.Range(-this.maxShift.y, this.maxShift.y);
+		float posZ = this.transform.parent.position.z + Random.Range(0, this.maxShift.z);
+		
+		/*if (posX + objectToPop.transform.localScale.x / 2 >= transformDesktop.position.x + transformDesktop.localScale.x / 2)
+			posX = transformDesktop.position.x + transformDesktop.localScale.x;
+		else if (posX - objectToPop.transform.localScale.x / 2 <= transformDesktop.position.x - transformDesktop.localScale.x / 2)
+			posX = transformDesktop.position.x + transformDesktop.localScale.x;*/
+		
+		if (posY + objectToPop.transform.localScale.y / 2 >= transformDesktop.position.y + transformDesktop.localScale.y / 2)
+			posY = transformDesktop.position.y - transformDesktop.localScale.y / 2 - objectToPop.transform.localScale.y / 2;
+		else if (posY - objectToPop.transform.localScale.y / 2 <= transformDesktop.position.y - transformDesktop.localScale.y / 2)
+			posY = transformDesktop.position.y + transformDesktop.localScale.y / 2 + objectToPop.transform.localScale.y / 2;
+		
+		Vector3 newPosition = new Vector3(posX, posY, posZ);
 		GameObject instantiate = Instantiate(objectToPop, newPosition, this.transform.parent.rotation) as GameObject;
 		instantiate.transform.parent = this.transform.parent.parent;
-		instantiate.GetComponent<PopupScript>().parentPopup = thisPopup;
-		thisPopup.counterChildPopup++;
 	}
 }
