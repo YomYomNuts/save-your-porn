ed68ab64c33104ebc7f52ee4c3481110b0308e6e
marechalbanane@gmail.com <marechalbanane@gmail.com@b7f410d1-8794-cbab-fdf4-a3e517f50e5e>
2013-01-27 12:10:38 +0000

    introPopWindow

--- trunk/Assets/Code/EffectIntro.cs
+++ trunk/Assets/Code/EffectIntro.cs
@@ -8,20 +8,24 @@ public class EffectIntro : MonoBehaviour {
 	public float timerAppearWindows = 2f;
 	
 	private float timer;
+	private bool done;
 
 	// Use this for initialization
 	void Start () {
 		window_porn.SetActive(false);
 		window_work.SetActive(false);
+		done = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		timer += Time.deltaTime;
-		if (timer >= timerAppearWindows)
+		if (timer >= timerAppearWindows && !done)
 		{
+			done = true;
 			window_porn.SetActive(true);
 			window_work.SetActive(true);
+			Camera.mainCamera.GetComponent<AudioSource>().Play();
 		}
 	}
 }
