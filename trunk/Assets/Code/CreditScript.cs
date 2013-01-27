a71dcd33bc0af6fbf63d64e99938e744393bf234
marechalbanane@gmail.com <marechalbanane@gmail.com@b7f410d1-8794-cbab-fdf4-a3e517f50e5e>
2013-01-27 13:43:36 +0000

    credit script

---
+++ trunk/Assets/Code/CreditScript.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections;
+
+public class CreditScript : MonoBehaviour
+{
+	GameObject movieGao;
+	
+	public void Start()
+	{
+		if (movieGao)
+		{
+			MovieTexture movTex = movieGao.renderer.material.mainTexture as MovieTexture;
+			if (!movTex.isPlaying)
+			{
+				movTex.loop = false;
+				movTex.Play();
+			}
+		}
+	}
+	
+	public void Update()
+	{
+		if (movieGao)
+		{
+			MovieTexture movTex = movieGao.renderer.material.mainTexture as MovieTexture;
+			if (movTex.isPlaying)
+				Application.Quit();
+		}
+	}
+}
+
