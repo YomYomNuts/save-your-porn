570b9207cb7ab212f757c4e78a4bf818c1cfb2cd
noisette.guillaume <noisette.guillaume@b7f410d1-8794-cbab-fdf4-a3e517f50e5e>
2013-01-27 07:07:01 +0000

    Objectif

--- trunk/Assets/Code/Objective.cs
+++ trunk/Assets/Code/Objective.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 using System.Collections;
 
 public class Objective : MonoBehaviour {
+	// Attributs
+	public float limitTimer = -1;
+	private float timer;
+	public Const.END_ACTION_TYPE actionWin;
+	public Const.LEVELS levelLoadIfWin;
+	public GameObject objectTakeActionIfWin;
 
 	// Use this for initialization
 	void Start () {
@@ -10,7 +16,14 @@ public class Objective : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		updateFunction();
+	}
 	
+	public void updateFunction()
+	{
+		timer += Time.deltaTime;
+		if (timer > this.limitTimer && this.limitTimer != - 1)
+			Application.LoadLevel((int)Const.LEVELS.LEVEL_LOSE - 1);
 	}
 	
 	public virtual void Init() {}
@@ -18,5 +31,25 @@ public class Objective : MonoBehaviour {
 	public virtual float GetCompletionFactor()
 	{
 		return 0;
+	}
+	
+	protected void Win()
+	{
+		switch(actionWin)
+		{
+		case Const.END_ACTION_TYPE.LOAD_LEVEL:
+			Application.LoadLevel((int)levelLoadIfWin - 1);
+			break;
+		case Const.END_ACTION_TYPE.DO_COMPRESSIONS:
+			objectTakeActionIfWin.GetComponent<StaticMassageObjective>().enabled = true;
+			break;
+		case Const.END_ACTION_TYPE.DO_INSUFFLATIONS:
+			objectTakeActionIfWin.GetComponent<MicHandle>().enabled = true;
+			objectTakeActionIfWin.GetComponent<BreathScript>().enabled = true;
+			break;
+		case Const.END_ACTION_TYPE.DO_MASSAGE:
+			objectTakeActionIfWin.GetComponent<CircularMassageObjective>().enabled = true;
+			break;
+		}
 	}
 }
